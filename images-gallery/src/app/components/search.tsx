572e873c103d576f8926968aabd2dec2988b92f3
hamdiaya <ay.hamdi@esi-sba.dz>
2024-08-05 15:49:17 +0200

    images gallery done

--- images-gallery/src/app/components/search.tsx
+++ images-gallery/src/app/components/search.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { useState,FormEvent } from 'react'
 import { useRouter } from 'next/navigation'
 
-export default function search ()  {
+export default function Search ()  {
     const [search,setSearch]=useState('');
     const router=useRouter();
     const handleSubmit=(e:FormEvent<HTMLFormElement>)=>{
